32be99b70e312a08a99a17b442f832eac3423948
ivantomicic <ivan@tomicic.dev>
2023-08-28 14:38:13 +0200

    added button

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,17 @@
-import '../faust.config';
-import React from 'react';
-import { useRouter } from 'next/router';
-import { FaustProvider } from '@faustwp/core';
-import '@faustwp/core/dist/css/toolbar.css';
-import '../styles/global.scss';
+import "../faust.config";
+import React from "react";
+import { useRouter } from "next/router";
+import { FaustProvider } from "@faustwp/core";
+import "@faustwp/core/dist/css/toolbar.css";
+import "../styles/global.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <FaustProvider pageProps={pageProps}>
-      <Component {...pageProps} key={router.asPath} />
-    </FaustProvider>
-  );
+	return (
+		<FaustProvider pageProps={pageProps}>
+			<button>Click This Button For Alert</button>
+			<Component {...pageProps} key={router.asPath} />
+		</FaustProvider>
+	);
 }
