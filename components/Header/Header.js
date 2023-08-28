e0d873c06be354de58fecbbad4f67faa34f7ff66
ivantomicic <ivan@tomicic.dev>
2023-08-28 14:56:50 +0200

    proper components

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,45 +1,51 @@
-import { useState } from 'react';
-import classNames from 'classnames/bind';
-import Link from 'next/link';
-import { Container, NavigationMenu, SkipNavigationLink } from '../../components';
-import styles from './Header.module.scss';
+import { gql } from "@apollo/client";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../assets/images/logo.svg";
 
-let cx = classNames.bind(styles);
+export default function Header(props) {
+	const { mainNavigation: nav } = props?.props.themeSettings;
 
-export default function Header({
-  title = 'Headless by WP Engine',
-  description,
-  menuItems
-}) {
-  const [isNavShown, setIsNavShown] = useState(false);
+	return (
+		<header className="site-navigation">
+			<nav className="container">
+				<Link href="/">
+					<Image priority src={logo} alt="Driven.World" />
+				</Link>
 
-  return (
-    <header className={cx('component')}>
-      <SkipNavigationLink />
-        <Container>
-          <div className={cx('navbar')}>
-            <div className={cx('brand')}>
-              <Link href="/">
-                <a className={cx('title')}>{title}</a>
-              </Link>
-              {description && <p className={cx('description')}>{description}</p>}
-            </div>
-            <button
-              type="button"
-              className={cx('nav-toggle')}
-              onClick={() => setIsNavShown(!isNavShown)}
-              aria-label="Toggle navigation"
-              aria-controls={cx('primary-navigation')}
-              aria-expanded={isNavShown}
-            >
-              ☰
-            </button>
-            <NavigationMenu
-              className={cx(['primary-navigation', isNavShown ? 'show' : undefined])}
-              menuItems={menuItems}
-            />
-        </div>
-      </Container>
-    </header>
-  );
+				<ul className="nav">
+					{nav?.map((item, index) => {
+						const link = item.link;
+						let btnClass = "btn btn--small";
+						if (index === 0) btnClass += " btn--tertiary";
+						if (index === 1) btnClass += " btn--primary";
+
+						return (
+							<li key={index}>
+								<Link href={link.url} className={btnClass}>
+									{link.title}
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
+		</header>
+	);
 }
+
+Header.fragments = {
+	entry: gql`
+		fragment HeaderFragment on DrivenThemeSettings {
+			themeSettings {
+				mainNavigation {
+					link {
+						title
+						url
+						target
+					}
+				}
+			}
+		}
+	`,
+};
