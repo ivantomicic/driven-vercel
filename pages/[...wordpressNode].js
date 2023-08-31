8839944c1025f8339789ad4d3448f5677471f728
ivantomicic <ivan@tomicic.dev>
2023-09-01 00:56:24 +0200

    revalidate30

--- pages/[...wordpressNode].js
+++ pages/[...wordpressNode].js
@@ -4,13 +4,8 @@ export default function Page(props) {
 	return <WordPressTemplate {...props} />;
 }
 
-export async function getStaticProps(ctx) {
-	const props = await getWordPressProps({ ctx });
-
-	return {
-		props,
-		revalidate: 60, // Re-generate the page every 60 seconds
-	};
+export function getStaticProps(ctx) {
+	return getWordPressProps({ ctx, revalidate: 30 });
 }
 
 export async function getStaticPaths() {
